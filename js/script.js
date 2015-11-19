0f76c6a4e5c3b66e3e4309ada015e8b8a0aa5578
jeremygholland <jeremygholland@gmail.com>
2015-11-18 18:42:21 -0600

    attempting to add modals, ineffective thus far

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	 $('.trigger1').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .1, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+  });
+	});
